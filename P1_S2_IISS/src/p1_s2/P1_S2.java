fa6f18b60373467a74f8d1888e00f338c9ad92ed
bugatti17 <36189709+bugatti17@users.noreply.github.com>
2018-02-20 12:12:09 +0100

    Subida

--- P1_S2_IISS/src/p1_s2/P1_S2.java
+++ P1_S2_IISS/src/p1_s2/P1_S2.java
@@ -6,16 +6,18 @@ import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/HoraServlet")
+@WebServlet("/P1_S2")
 public class P1_S2 extends HttpServlet {
 protected void doGet(HttpServletRequest request, HttpServletResponse response)
 throws ServletException, IOException {
 HttpSession sesion = request.getSession(true);
+sesion.setMaxInactiveInterval(10);
 response.setContentType("text/html");
 PrintWriter html = response.getWriter();
 html.print("<strong>");
@@ -30,5 +32,6 @@ date = new Date();
 sesion.setAttribute("date", date);
 html.print("Fecha actual: " + date);
 html.print("</strong>");
+
 }
 }
